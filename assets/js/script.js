555842c939aecd7af17e025e5ce8108fad870aaf
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-21 17:00:29 -0500

    moment.js features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+    var date = $(taskEl).find("span").text().trim();
+    
+
+    var time = moment(date, "L").set("hour", 17);
+
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskEl).addClass("list-group-item-warning");
+    }
+    
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,7 +30,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -97,10 +114,16 @@ $(".list-group").on("click", "span", function() {
     // swap out elements
     $(this).replaceWith(dateInput);
     //automatically focus on new element
+    dateInput.datepicker({
+        minDate: 1,
+        onClose: function() {
+            $(this).trigger("change");
+        }
+    });
     dateInput.trigger("focus");
 });
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
     // get current text
     var date = $(this)
       .val()
@@ -128,6 +151,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
   
     // replace input with span element
     $(this).replaceWith(taskSpan);
+
+    auditTask($(taskSpan).closest(".list-group-item"));
   });
 
 
@@ -237,4 +262,9 @@ $(".card .list-group").sortable({
       }
   });
 
+  $("#modalDueDate").datepicker({
+      minDate: 1
+  });
 
+
+// moment function 
