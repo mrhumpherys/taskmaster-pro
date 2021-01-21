d67410b3477a7c0fc18f83cbfdf71fb2efafc877
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-21 18:25:14 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var auditTask = function(taskEl) {
     else if (Math.abs(moment().diff(time, "days")) <= 2) {
         $(taskEl).addClass("list-group-item-warning");
     }
-    
+    console.log(taskEl);
 };
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -170,7 +170,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -209,16 +209,20 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-      console.log("activate", this);
+      $(this).addClass("dropover");
+      $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
-      console.log("deactivate", this);
+      $(this).removeClass("dropover");
+      $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
-      console.log("over", event.target);
+      $(event.target).addClass("dropover-active");
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event) {
-      console.log("out", event.target);
+      $(event.target).removeClass("dropover-active");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     update: function(event) {
         var tempArr = [];
@@ -267,4 +271,8 @@ $(".card .list-group").sortable({
   });
 
 
-// moment function 
+setInterval(function(){
+    $(".card .list-group-item").each(function(index, el){
+        auditTask(el);
+    });
+}, 1800000); 
